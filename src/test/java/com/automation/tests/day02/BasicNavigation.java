87f6989a9da18fc956b92ae9e01c8e09e11eae45
Yerkhanat <erha.mts@gmail.com>
2020-03-01 12:23:34 -0800

    new method added

--- src/test/java/com/automation/tests/day02/BasicNavigation.java
+++ src/test/java/com/automation/tests/day02/BasicNavigation.java
@@ -12,22 +12,32 @@ public class BasicNavigation {
         WebDriverManager.chromedriver().setup();
         WebDriver driver  = new ChromeDriver();
         driver.get("http://google.com");
-
-
         Thread.sleep(5000);
       if(driver.getTitle().equals("Google")){
             System.out.println("Title test is passed");
         }else{
           System.out.println("Test test is failed");
       }
-        driver.close();
+      driver.navigate().to("http://amazon.com");
+      if(driver.getTitle().toLowerCase().contains("amazon")){
+          System.out.println("Title test is passed for amazon");
+      }else{
+          System.out.println("Test test is failed");
+      }
+      driver.navigate().back();
+      verifyEquals(driver.getTitle(),"Google");
 
-
-
-
-
-
-
+      //=========================
+      driver.close();
 
     }
+
+    public static void verifyEquals(String arg1, String arg2){
+        if(arg1.equals(arg2)){
+            System.out.println("Title test is passed");
+        }else{
+            System.out.println("Test test is failed");
+        }
+    }
+
 }
