1edbc3116196c38e8d09e9174c16e0f6f96b839c
Yerkhanat <erha.mts@gmail.com>
2020-03-01 10:29:13 -0800

    Created Basic Navigation

---
+++ src/test/java/com/automation/tests/day02/BasicNavigation.java
@@ -0,0 +1,20 @@
+package com.automation.tests.day02;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class BasicNavigation {
+    public static void main(String[] args) {
+        // to start selenium script we need:
+        // setup webDriver and create webDriver object
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver  = new ChromeDriver();
+        driver.get("http://google.com");
+
+
+
+
+    }
+}
