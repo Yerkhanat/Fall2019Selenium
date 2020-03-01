13b6b4913ad21db61f139a96e8319a8d156e5631
Yerkhanat <erha.mts@gmail.com>
2020-03-01 10:43:32 -0800

    added close method

--- src/test/java/com/automation/tests/day02/BasicNavigation.java
+++ src/test/java/com/automation/tests/day02/BasicNavigation.java
@@ -6,12 +6,16 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class BasicNavigation {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception{
         // to start selenium script we need:
         // setup webDriver and create webDriver object
         WebDriverManager.chromedriver().setup();
         WebDriver driver  = new ChromeDriver();
         driver.get("http://google.com");
+        Thread.sleep(3000);
+        driver.close();
+
+
 
 
 
