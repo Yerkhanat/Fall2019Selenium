78377dc507f9d10f2d76f485a7718d573be8cc1b
Yerkhanat <erha.mts@gmail.com>
2020-03-07 17:13:11 -0800

    Homework-1

---
+++ src/test/java/com/automation/tests/homework/BasicNavigationTests.java
@@ -0,0 +1,29 @@
+package com.automation.tests.homework;
+
+import com.automation.utilities.BrowserUtils;
+import com.automation.utilities.DriverFactory;
+import com.automation.utilities.StringUtility;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class BasicNavigationTests {
+    public static void main(String[] args) {
+       WebDriver driver = DriverFactory.CreateDriver("safari");
+       //WebDriver driver =DriverFactory.CreateDriver("chrome");
+
+        driver.get("http://www.google.com");
+        String titleGoogle = driver.getTitle();
+        driver.get("http://www.etsy.com");
+        String titleEtsy = driver.getTitle();
+        BrowserUtils.wait(2);
+        driver.navigate().back();
+       StringUtility.verifyEquals(titleGoogle,titleEtsy);
+       driver.navigate().forward();
+       StringUtility.verifyEquals(titleEtsy,driver.getTitle());
+       driver.quit();
+
+
+
+    }
+}
